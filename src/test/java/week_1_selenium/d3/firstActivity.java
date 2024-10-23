1755a623f168093836d65077a9ced4b2890d6bfd
Mhammad <moh.amini2323@gmail.com>
2024-10-23 19:43:42 -0400

    d3 firstA

--- src/test/java/week_1_selenium/d3/firstActivity.java
+++ src/test/java/week_1_selenium/d3/firstActivity.java
@@ -5,18 +5,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class firstActivity {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws InterruptedException {
         ChromeDriver driver=new ChromeDriver();
         driver.get("https://retail.tekschool-students.com/");
         By signInLocator=By.id("signinLink");
         driver.findElement(signInLocator).click();
         By emailBorderLocator=By.id("email");
-        driver.findElement(emailBorderLocator).sendKeys("myemsislkdk");
+        driver.findElement(emailBorderLocator).sendKeys("myemsislkdk@gmail.com");
         By passaBorderLocator=By.id("password");
         driver.findElement(passaBorderLocator).sendKeys("passkjfklkkgl;s");
         By logInLocatorBOTTON=By.id("loginBtn");
         driver.findElement(logInLocatorBOTTON).click();
-        By messageLocator=By.className("wrong username or password");
+        Thread.sleep(1000);
+        By messageLocator=By.className("error");
         String messagTAEXT=driver.findElement(messageLocator).getText();
         System.out.println(messagTAEXT);
         driver.quit();
