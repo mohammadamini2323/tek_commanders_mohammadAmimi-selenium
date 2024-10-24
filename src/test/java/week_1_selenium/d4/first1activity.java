81574085a82dbf651fbb964bd480d52299ae7287
Mhammad <moh.amini2323@gmail.com>
2024-10-24 19:36:26 -0400

    d3 firstA

--- src/test/java/week_1_selenium/d4/first1activity.java
+++ src/test/java/week_1_selenium/d4/first1activity.java
@@ -4,14 +4,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Scanner;
+
 public class first1activity {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("for chrome driver inter 1   and   for edge driver inter 2");
+        int userInput =scanner.nextInt();
+        if (userInput==1){
         ChromeDriver chromeDriver=new ChromeDriver();
         chromeDriver.get("https://dev.retail.tekschool-students.com/");
-        chromeDriver.quit();
+        String Chrometitle= chromeDriver.getTitle();
+        System.out.println(Chrometitle);
+            System.out.println("it was chrome");
+
+        chromeDriver.quit();} else if (userInput==2) {
+
+
+
         EdgeDriver edgeDriver=new EdgeDriver();
         edgeDriver.get("https://dev.retail.tekschool-students.com/");
-        edgeDriver.quit();
+        String egedrivertitle=edgeDriver.getTitle();
+        System.out.println(egedrivertitle);
+            System.out.println("it was edge");
+        edgeDriver.quit();}else {
+            System.out.println("wrong enteery");}
 
     }
 }
